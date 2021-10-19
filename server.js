ef8559802f4e1eb85d2e7f7c19634fc66dc6db18
Mukul Kolpe <mukulkolpe45@gmail.com>
2021-10-19 23:53:22 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const httpServer= require("http").createServer();
+const io=require("socket.io")(httpServer,{
+    cors: {
+        origin: "http://localhost:3000",
+    },
+}
+
+);
+
+io.on("connection", (socket)=>{
+    socket.on("chat", (args) => {
+        socket.broadcast.emit("chat", args);
+    })
+})
+
+httpServer.listen(3001);
--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import {io} from "socket.io-client";
+
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { Timeline } from "primereact/timeline";
+
+const socket=io("http:localhost:3001");
 
 function App() {
+  const [author, setAuthor] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([
+    { author: "Bot", message: "Start Chatting!!" },
+  ]);
+
+socket.on("chat", (args)=> setMessages([...messages, args]));
+
+  const addMessage = () => {
+    const send = { author, message };
+    setMessages([...messages, send]);
+    setMessage("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="p-col-12 p-md-4">
+        <div className="p-inputgroup">
+          <span className="p-inputgroup-addon">
+            <i className="pi pi-user"></i>
+          </span>
+          <InputText placeholder="Name" value={author} onChange={(e)=> setAuthor(e.target.value)}/>
+          <InputText placeholder="Message" value={message} onChange={(e)=> setMessage(e.target.value)}/>
+          <Button label="send" onClick={()=> addMessage()}/>
+        </div>
+      </div>
+      <div className="card">
+        <h5>Conversation</h5>
+        <Timeline 
+        value={messages}
+        opposite={(item) => item.author}
+        content={(item)=>{
+          <small className="p-text-secondary">{item.message}</small>
+        }}/>
+      </div>
     </div>
   );
 }
